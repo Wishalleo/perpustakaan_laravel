e760f3d244964c5300087f28a726faf5f5865258
Wishalleo <waridhil@gmail.com>
2022-10-24 20:11:30 +0700

    first commit

---
+++ app/Http/Controllers/DetailBorrowController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\detail_borrow;
+use Illuminate\Http\Request;
+
+class DetailBorrowController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\detail_borrow  $detail_borrow
+     * @return \Illuminate\Http\Response
+     */
+    public function show(detail_borrow $detail_borrow)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\detail_borrow  $detail_borrow
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(detail_borrow $detail_borrow)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\detail_borrow  $detail_borrow
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, detail_borrow $detail_borrow)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\detail_borrow  $detail_borrow
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(detail_borrow $detail_borrow)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_10_24_125312_create_borrows_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('borrows', function (Blueprint $table) {
+            $table->id();
+            $table->string('code_borrow');
+            $table->string('operator');
+            $table->string('status');
+            $table->string('forfeit');
+            $table->date('borrow_date');
+            $table->date('return_date');
+            $table->date('date_of_return');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('borrows');
+    }
+};
